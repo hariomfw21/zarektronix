b53fd7f9b7371b182fc25fc4ad269e3c10ec79ee
hariomfw21 <darkphinixgamer@gmail.com>
2023-12-03 20:58:22 +0530

    assignment done

---
+++ README.md
@@ -0,0 +1,59 @@
+# Joke CLI Tool with Leaderboard Feature
+
+Welcome to the Joke CLI Tool, a fun and interactive command line application that allows you to search for jokes, save them, and even discover the most liked jokes in a leaderboard.
+
+## Table of Contents
+
+Introduction
+Features
+Requirements
+Usage
+
+### Introduction
+
+This CLI tool is designed to provide a unique and humorous experience by interacting with the 'icanhazdadjoke' API. It not only fetches jokes based on your search term but also lets you save and like jokes, creating a leaderboard of the most popular ones.
+
+### Features
+Search for Jokes: Use the --search command followed by your search term to fetch jokes.
+
+Save and Like Jokes: When a joke is displayed, you can choose to like it or not. Liked jokes are saved and contribute to the leaderboard.
+
+Leaderboard: Use the --leaderboard command to view the most liked jokes in a leaderboard format.
+
+###  Clone the Repository:
+
+To get started, clone the Git repository using the following li
+git clone https://github.com/hariomfw21/zarektronix.git
+Run the Program:
+
+Navigate to the project directory:
+
+cd zarektronix
+
+Run the program using Node.js:
+
+node index.js --search [your search term]
+
+Replace [your search term] with the term you want to search for jokes. For example:
+
+node index.js --search programming
+
+Leaderboard Feature:
+
+To view the leaderboard, use the following command:
+
+node index.js --leaderboard
+Explore the leaderboard to discover the most liked jokes.
+
+Contributing:
+
+If you'd like to contribute to the project, feel free to open issues or submit pull requests. Check the project's repository on GitHub for more details.
+
+
+Thank you for using Zarektronix. Have fun exploring jokes and interacting with the leaderboard!
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// Joke CLI Tool with Leaderboard Feature
+
 const fs = require("fs");
 
+// API URL for fetching jokes
 const API_URL = "https://icanhazdadjoke.com/search";
+
+
+//  Fetch jokes from the 'icanhazdadjoke' API based on the provided search term.
 
 const fetchJokes = async (searchTerm) => {
   try {
@@ -21,6 +27,9 @@ const fetchJokes = async (searchTerm) => {
   }
 };
 
+
+//   Select a random joke from the provided array of jokes.
+
 const selectRandomJoke = (jokes) => {
   if (jokes.length > 0) {
     const randomIndex = Math.floor(Math.random() * jokes.length);
@@ -30,26 +39,35 @@ const selectRandomJoke = (jokes) => {
   }
 };
 
+
+//  Save a joke to the 'jokes.txt' file, updating likes if the joke already exists.
+
 const saveJokeToFile = (joke, jokes, liked) => {
   const existingJokeIndex = jokes.findIndex(
     (existing) => existing.joke === joke.joke
   );
 
   if (existingJokeIndex !== -1) {
+    // Update likes for the existing joke
     jokes[existingJokeIndex].likes =
       (jokes[existingJokeIndex].likes || 0) + (liked ? 1 : 0);
   } else {
-    const newJoke = { joke: joke.joke, likes: liked ? 1 : 0 };
-    jokes.push(newJoke);
+    // Add a new joke with 0 likes
+    jokes.push({ joke: joke.joke, likes: liked ? 1 : 0 });
   }
 
+  // Save the updated jokes array to 'jokes.txt'
   fs.writeFileSync("jokes.txt", JSON.stringify(jokes, null, 2), "utf8");
 };
+
+
+//  Display a witty message when no jokes are available. 
 
 const displayWittyMessage = () => {
   console.log("the joke gods are taking a day off.");
 };
 
+//  Prompt the user to indicate whether they liked the displayed joke.
 const askForLike = () => {
   return new Promise((resolve) => {
     const readline = require("readline").createInterface({
@@ -64,29 +82,41 @@ const askForLike = () => {
   });
 };
 
+
+//  Display the leaderboard with the most liked jokes.
+
 const displayLeaderboard = (jokes) => {
   console.log("Displaying leaderboard...");
 
+  // Sort jokes based on likes in descending order
   const sortedJokes = jokes.sort((a, b) => (b.likes || 0) - (a.likes || 0));
 
+  // Display the sorted jokes with their respective likes
   sortedJokes.forEach((joke, index) => {
     console.log(`${index + 1}. Joke: ${joke.joke}, Likes: ${joke.likes || 0}`);
   });
 };
 
+/**
+ * Main function to handle user commands and execute the appropriate actions.
+ */
 const main = async () => {
+  // Retrieve command line arguments
   const args = process.argv.slice(2);
 
+  // Check if any command line arguments are provided
   if (args.length === 0) {
-    console.log(
-    );
+    console.log("Invalid command: Please provide a search term using --search or use the --leaderboard option.");
     return;
   }
 
+  // Extract the command from the arguments
   const command = args[0];
 
+  // Initialize an array to store jokes
   let jokes = [];
 
+  // Try to read existing jokes from 'jokes.txt'
   try {
     const jokesData = fs.readFileSync("jokes.txt", "utf8");
     jokes = JSON.parse(jokesData);
@@ -94,43 +124,44 @@ const main = async () => {
     console.error("Error reading jokes file:", error.message);
   }
 
+  // Handle different commands
   if (command === "--search" && args.length > 1) {
+    // Search for jokes based on the provided search term
     const searchTerm = args.slice(1).join(" ");
     try {
       const jokesFromAPI = await fetchJokes(searchTerm);
       const selectedJoke = selectRandomJoke(jokesFromAPI);
 
       if (selectedJoke) {
+        // Display the selected joke
         console.log(`Here's a joke for you: ${selectedJoke.joke}`);
 
+        // Ask the user if they liked the joke
         const likeResponse = await askForLike();
 
-        saveJokeToFile(
-          selectedJoke,
-          jokes,
-          likeResponse.toLowerCase() === "yes"
-        );
+        // Save the joke to 'jokes.txt' and update likes based on user response
+        saveJokeToFile(selectedJoke, jokes, likeResponse.toLowerCase() === "yes");
 
         if (likeResponse.toLowerCase() === "yes") {
-          console.log(
-            "Thanks for liking the joke! It has been added to the leaderboard."
-          );
+          console.log("Thanks for liking the joke! It has been added to the leaderboard.");
         } else {
           console.log("No worries, maybe next time!");
         }
       } else {
+        // Display a witty message if no jokes are available
         displayWittyMessage();
       }
     } catch (error) {
       console.error(error.message);
     }
   } else if (command === "--leaderboard") {
+    // Display the leaderboard based on the stored jokes
     displayLeaderboard(jokes);
   } else {
-    console.log(
-      "Invalid command : Please provide a search term using --search or use the --leaderboard option."
-    );
+    // Display an error message for invalid commands
+    console.log("Invalid command: Please provide a search term using --search or use the --leaderboard option.");
   }
 };
 
+// Execute the main function
 main();
